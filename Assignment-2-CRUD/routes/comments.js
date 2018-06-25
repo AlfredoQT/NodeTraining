25bbbb1aeeb723188dfa83b97ea8cd8c145e2986
Alfredo Quintero Tlacuilo <alfredoq98@hotmail.com>
2018-06-25 10:41:38 -0700

    Ready for testing

--- Assignment-2-CRUD/routes/comments.js
+++ Assignment-2-CRUD/routes/comments.js
@@ -37,4 +37,28 @@ router.post( '/', (req, res) => {
     res.status( 201 ).send( { postId: req.accessedPostID, commentId: req.params.id} );
 });
 
+// Update one
+router.put( '/:id', idBoundsChecker, ( req, res ) => {
+    if ( !req.body.text ) {
+        return res.status( 400 ).send( { error: "Data incorrect" } );
+    }
+
+    // Construct it
+    const comment = {
+        text: req.body.text
+    };
+
+    // Update it
+    store.posts[req.accessedPostID].comments[req.params.id] =  comment;
+
+    res.status( 204 ).send();
+});
+
+// Delete one
+router.delete( '/:id', idBoundsChecker, ( req, res ) => {
+    // Splice the guy
+    store.posts[req.accessedPostID].comments.splice( req.params.id, 1 );
+    res.send( 204 ).send();
+});
+
 module.exports = router;
