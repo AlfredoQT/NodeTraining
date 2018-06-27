6d1a4fba2ac17db21469d1caebe30c2d941741a2
Alfredo Quintero Tlacuilo <alfredoq98@hotmail.com>
2018-06-27 14:17:08 -0700

    Added put and delete for account by id

--- Assignment-4-Mongoose/routes/accounts.js
+++ Assignment-4-Mongoose/routes/accounts.js
@@ -2,6 +2,13 @@ const express = require( 'express' );
 const router = express.Router();
 const Account = require( '../lib/account-model' );
 
+const paramsCheck = ( req, res, next ) => {
+    if ( !req.body.name || !req.body.balance || Number.isNaN( Number.parseFloat( req.body.balance ) ) ) {
+        return res.status( 400 ).send( { error: 'Wrong data' } );
+    }
+    next();
+};
+
 router.get( '/', ( req, res ) => {
     // Retrieve by id in descending order
     Account
@@ -15,16 +22,33 @@ router.get( '/', ( req, res ) => {
     });
 });
 
-router.post( '/', ( req, res ) => {
-    if ( !req.body.name || !req.body.balance || Number.isNaN( Number.parseFloat( req.body.balance ) ) ) {
-        return res.status( 400 ).send( { error: 'Wrong data' } );
-    }
+router.post( '/', paramsCheck, ( req, res ) => {
     let newAccount = new Account( { name: req.body.name, balance: Number.parseFloat( req.body.balance ) } );
     newAccount.save( ( err ) => {
         if ( err ) {
             return res.status( 500 ).send();
         }
         res.status( 201 ).send( { createdAccount: newAccount } );
+    });
+});
+
+router.put( '/:id', paramsCheck, ( req, res ) => {
+    Account.findByIdAndUpdate( req.params.id, { $set: { name: req.body.name, balance: Number.parseFloat( req.body.balance ) } }, ( err, result ) => {
+        if ( err ) {
+            return res.status( 500 ).send();
+        }
+        console.log( req.body.balance );
+        res.status( 200 ).send( { updatedAccount: result } );
+    });
+});
+
+router.delete( '/:id', paramsCheck, ( req, res ) => {
+    Account.findByIdAndDelete( req.params.id, ( err, result ) => {
+        if ( err ) {
+            return res.status( 500 ).send();
+        }
+        console.log( req.body.balance );
+        res.status( 200 ).send( { deletedAccount: result } );
     });
 });
 
